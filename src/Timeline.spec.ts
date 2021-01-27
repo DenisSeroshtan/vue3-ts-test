64d30074a6652c3c32a41957b7ea362be4719106
DenisSeroshtan <haron_777@mail.ru>
2021-01-28 00:09:52 +0500

    trigger click to tab period

--- src/Timeline.spec.ts
+++ src/Timeline.spec.ts
@@ -2,10 +2,30 @@ import {mount} from '@vue/test-utils'
 import Timeline from "./Timeline.vue"
 
 describe('Timeline', () => {
+
     it('render 3 timeline', () => {
         const wrapper = mount(Timeline)
-        const links = wrapper.findAll('a')
+        const links = wrapper.findAll('[data-test="period"]')
 
         expect(links).toHaveLength(3)
+    })
+
+    it('updated period when click', async () => {
+        const wrapper = mount(Timeline)
+
+        const $today = wrapper.findAll('[data-test="period"]')[0]
+        expect($today.classes()).toContain('is-active')
+
+        const $week = wrapper.findAll('[data-test="period"]')[1]
+        await $week.trigger('click')
+
+        expect($week.classes()).toContain('is-active')
+        expect($today.classes()).not.toContain('is-active')
+
+        const $month = wrapper.findAll('[data-test="period"]')[2]
+        await $month.trigger('click')
+
+        expect($month.classes()).toContain('is-active')
+        expect($week.classes()).not.toContain('is-active')
     })
 })
--- src/Timeline.vue
+++ src/Timeline.vue
@@ -1,21 +1,33 @@
 <template>
   <nav class="is-primary panel">
     <p class="panel-tabs">
-      <a v-for="period in periods" :key="period" href="#"> {{ period }}</a>
+      <a
+        v-for="period in periods"
+        :key="period"
+        :class="selectedPeriod === period ? 'is-active' : ''"
+        href="#"
+        data-test="period"
+        @click="setPeriod(period)"
+
+      > {{ period }}</a>
     </p>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Period } from './types'
 
 export default defineComponent({
   setup() {
     const periods: Period[] = ['сегодня', 'на этой неделе', 'в этот месяц']
+    const selectedPeriod = ref<Period>('сегодня')
+    const setPeriod = (period: Period) => selectedPeriod.value = period
 
     return {
       periods,
+      selectedPeriod,
+      setPeriod,
     }
   }
 })
