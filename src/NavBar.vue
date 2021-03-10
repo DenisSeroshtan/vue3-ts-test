9b2775dbfa5f37c7172c0b06e04d8b3de0438e90
DenisSeroshtan <haron_777@mail.ru>
2021-03-11 00:20:46 +0500

    добавли NavBar компонент

---
+++ src/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-end">
+      <router-link class="button" to="/posts/new">new post</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'NavBar',
+})
+</script>
+
---
+++ src/NewPost.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    new post
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'NewPost',
+})
+</script>
